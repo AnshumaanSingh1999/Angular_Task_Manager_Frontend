b7463d5fc29c8d38783f9d229a81fa9a096aef23
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-05-01 10:18:47 +0530

    home features

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public userdate:any
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.userdate=localStorage.getItem("usernamels") 
   }
 
+  logoutFunction(){
+    localStorage.clear()
+    this.router.navigate([""])
+  }
+  
 }
